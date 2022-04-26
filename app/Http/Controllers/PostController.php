e20dc70ae6605d652648cc996545579ec1e54390
Braulio Fausto <braulioffm@gmail.com>
2022-04-25 22:32:05 -0500

    comenzadno con roles y permisos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,6 +21,8 @@ class PostController extends Controller
 
     public function show(Post $post){
 
+        $this->authorize('published', $post);
+    
       $similares = Post::where('category_id', $post->category_id)
                            ->where('id', '!=', $post->id )
                            ->where('status', 2)
